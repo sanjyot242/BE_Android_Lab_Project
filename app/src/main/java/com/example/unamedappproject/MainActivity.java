0d48549c90873c16b8256c0d9f116cf60d281af9
sanjyot242 <sanjyot.satvi242.com>
2020-09-09 15:26:35 +0530

    Using Navigation Component Library  added bottom navigation toolbar and connected them to respective fragments

--- app/src/main/java/com/example/unamedappproject/MainActivity.java
+++ app/src/main/java/com/example/unamedappproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.unamedappproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
@@ -9,27 +10,35 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText inputUsernme,inputPassword;
+    private EditText inputUsernme, inputPassword;
     private Button signIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inputUsernme=findViewById(R.id.username);
-        inputPassword=findViewById(R.id.password);
-        signIn=findViewById(R.id.signIn);
+        inputUsernme = findViewById(R.id.username);
+        inputPassword = findViewById(R.id.password);
+        signIn = findViewById(R.id.signIn);
 
-        signIn.setOnClickListener(v->{
-                if(TextUtils.isEmpty(inputPassword.getText())||TextUtils.isEmpty(inputPassword.getText())){
-                    Toast.makeText(this, "Please Fill all details", Toast.LENGTH_SHORT).show();
-                }else{
-                    signIn();
-                }
+        signIn.setOnClickListener(v -> {
+            if (TextUtils.isEmpty(inputPassword.getText()) || TextUtils.isEmpty(inputPassword.getText())) {
+                Toast.makeText(this, "Please Fill all details", Toast.LENGTH_SHORT).show();
+            } else {
+                signIn(inputUsernme.getText().toString(), inputPassword.getText().toString());
+            }
         });
     }
 
-    private void signIn() {
-        Toast.makeText(this, inputUsernme.getText()+"\n"+inputPassword.getText(), Toast.LENGTH_SHORT).show();
+    private void signIn(String Username, String Password) {
+        if (Username.equals("Sanjyot") && Password.equals("123456")) {
+            startActivity(new Intent(this,MainHostActivity.class));
+            Toast.makeText(this, inputUsernme.getText() + "\n" + inputPassword.getText(), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Wrong password", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }

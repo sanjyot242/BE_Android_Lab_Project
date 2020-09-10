987e3691037db8741ed6e946c412b3edcdf1e154
sanjyot242 <sanjyot.satvi242.com>
2020-09-10 11:30:30 +0530

    Firebase

--- app/src/main/java/com/example/unamedappproject/MainActivity.java
+++ app/src/main/java/com/example/unamedappproject/MainActivity.java
@@ -1,22 +1,35 @@
 package com.example.unamedappproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.play.core.tasks.OnCompleteListener;
+import com.google.android.play.core.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
     private EditText inputUsernme, inputPassword;
     private Button signIn;
+    private FirebaseAuth mAuth;
+
+// Initialize Firebase Auth
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
         inputUsernme = findViewById(R.id.username);
         inputPassword = findViewById(R.id.password);
@@ -32,13 +45,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void signIn(String Username, String Password) {
-        if (Username.equals("Sanjyot") && Password.equals("123456")) {
-            startActivity(new Intent(this,MainHostActivity.class));
-            Toast.makeText(this, inputUsernme.getText() + "\n" + inputPassword.getText(), Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Wrong password", Toast.LENGTH_SHORT).show();
-        }
+//        if (Username.equals("Sanjyot") && Password.equals("123456")) {
+//            startActivity(new Intent(this,MainHostActivity.class));
+//            Toast.makeText(this, inputUsernme.getText() + "\n" + inputPassword.getText(), Toast.LENGTH_SHORT).show();
+//        } else {
+//            Toast.makeText(this, "Wrong password", Toast.LENGTH_SHORT).show();
+//        }
+        mAuth.signInWithEmailAndPassword(Username, Password)
+                .addOnCompleteListener(this, (com.google.android.gms.tasks.OnCompleteListener<AuthResult>) task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("TAG", "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("TAG", "signInWithEmail:failure", task.getException());
+                        Toast.makeText(MainActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        // ...
+                    }
 
 
+                    // ...
+                });
+
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+        startActivity(new Intent(getApplicationContext(),MainHostActivity.class));
+        finish();
     }
 }
